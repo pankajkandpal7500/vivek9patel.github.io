ea3b7a062dc1ac0473b569c8f77543e453c80bda
Pankaj kandpal <53327787+pankajkandpal7500@users.noreply.github.com>
2024-08-27 00:00:28 +0530

    Update vivek.js

--- components/apps/vivek.js
+++ components/apps/vivek.js
@@ -137,7 +137,7 @@ function Education() {
     return (
         <>
             <div className=" font-medium relative text-2xl mt-2 md:mt-4 mb-4">
-                Education
+               Pankaj Education
                 <div className="absolute pt-px bg-white mt-px top-full w-full">
                     <div className="bg-white absolute rounded-full p-0.5 md:p-1 top-0 transform -translate-y-1/2 left-full"></div>
                     <div className="bg-white absolute rounded-full p-0.5 md:p-1 top-0 transform -translate-y-1/2 right-full"></div>
@@ -146,7 +146,7 @@ function Education() {
             <ul className=" w-10/12  mt-4 ml-4 px-0 md:px-1">
                 <li className="list-disc">
                     <div className=" text-lg md:text-xl text-left font-bold leading-tight">
-                        Arizona State University
+                       GB PANT
                     </div>
                     <div className=" text-sm text-gray-400 mt-0.5">2022 - 2024</div>
                     <div className=" text-sm md:text-base">Computer Science</div>
